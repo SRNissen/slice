4bd6af19890e03fa27514906796e71029d5a6427
SRNissen <4851097+SRNissen@users.noreply.github.com>
2023-03-21 13:20:11 +0100

    mkdir missing dirs

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,38 +3,47 @@
 
 echo "script: clean.sh"
 
+mkdir -p obj
 cd obj
     touch cleaningdummy
     rm *
 cd ..
 
+mkdir -p bin
 cd bin
     touch cleaningdummy
     rm *
 cd ..
 
+mkdir -p san
 cd san
 
+    mkdir -p asan
     cd asan
 
+        mkdir -p bin
         cd bin
             touch cleaningdummy
             rm *
         cd ..
 
+        mkdir -p obj
         cd obj
             touch cleaningdummy
             rm *
         cd ..
     cd ..
     
+    mkdir -p msan
     cd msan
 
+        mkdir -p bin
         cd bin
             touch cleaningdummy
             rm *
         cd ..
 
+        mkdir -p obj
         cd obj
             touch cleaningdummy
             rm *
@@ -42,13 +51,16 @@ cd san
 
     cd ..
     
-        cd usan
+        mkdir -p usan
 
+        cd usan
+        mkdir -p bin
         cd bin
             touch cleaningdummy
             rm *
         cd ..
 
+        mkdir -p obj
         cd obj
             touch cleaningdummy
             rm *
